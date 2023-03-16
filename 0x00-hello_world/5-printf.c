9b97847eea0ccb1db56ec086759923d4c64aa7dc
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-03-16 14:20:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints a string
+ * Return: 0 if sucesses.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
