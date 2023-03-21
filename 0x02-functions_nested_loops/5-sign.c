f740db518998d21b5f9e00d6dcc4c18eb6bfef38
Abdulaziz7878 <125488334+Abdulaziz7878@users.noreply.github.com>
2023-03-22 00:22:35 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign - Determines if the input number greater, equal or less than zero. 
+ * print_sign - Determines if the input number greater,equal or less than zero.
  * @n: The character in asqii code.
  *Return: 1 is greater than zero. 0 is zero. -1 is less than zero.
  */
