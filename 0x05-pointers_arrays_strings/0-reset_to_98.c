f08fb9af11e292a78bb300095cdbdf3d2e048b0b
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-03-27 07:55:00 -0700

    takes a pointer to an intas parameter and updates the value it points to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h" 
  
 /**
- * reset_to_98 - takes a pointer
- *@n: pointer to n
+ * reset_to_98 - takes a pointer to an intas parameter and updates the value it points to 98
+ * @n: pointer to n
  */
   
 void reset_to_98(int *n)
