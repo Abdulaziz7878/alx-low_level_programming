15b455104d5d8295b645b6855d62ccd50a80ab29
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-03-27 23:36:12 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%d\n", rand() % INT_MAX);
+	return (0);
+}
+
