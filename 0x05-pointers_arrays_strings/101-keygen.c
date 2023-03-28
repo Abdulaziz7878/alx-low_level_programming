d944bbe279835ce6f676d066222354b110497125
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-03-28 00:06:39 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,11 +13,12 @@ int main(void)
 	int myrand;
 	int count;
 	int total;
+
 	srand(time(NULL));
 	for (count = 0, total = 2772; total > 122; count++)
 	{
 		myrand = (rand() % 125) + 1;
-		printf("%c", myrand;
+		printf("%c", myrand);
 				total -= myrand;
 				}
 				printf("%c", total);
