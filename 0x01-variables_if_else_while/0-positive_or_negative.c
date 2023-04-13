0b4b22709bcf3567811691a27db13ea4972ccce6
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-04-12 17:36:10 -0700

    0-malloc_checked.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n>0){
-	printf("is positive");
+	printf(n,"is positive");
 	}else if (n==0){
-	printf("is zero");
+	printf(n,"is zero");
 	}else {
-	printf("is negative");
+	printf(n,"is negative");
 	}
 	return (0);
 }
