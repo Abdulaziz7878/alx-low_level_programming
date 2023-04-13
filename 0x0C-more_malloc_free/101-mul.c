e5ed4486ad378913b7ecc8d3bf1bc4e582c80e62
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-04-12 18:58:09 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,122 +1,97 @@
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
-/* First I am Proud of what i did here */
+#define ERR_MSG "Error"
 
 /**
- * checkNum - this function check if the strings passed as an arguments
- * if all the strings are digits the everything is fine
- * else it print Error and exit with the code of 98
- * @str1: the first stirng
- * @str2: the second string
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: nothing just checking
-*/
-int checkNum(char *str1, char *str2)
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	int len1 = strlen(str1) - 1, len2 = strlen(str2) - 1, i = 0;
+	int i = 0;
 
-	for (i = 0; i < len1; i++)
+	while (s[i])
 	{
-		if (!isdigit(str1[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+				i++;
 	}
-
-	for (i = 0; i < len2; i++)
-	{
-		if (!isdigit(str2[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-
-	return (0);
-}
-
-/**
- * initMemory - this function makes memory for the result
- * @size: is the size of length of two strings
- * Return: a pointer the the first elements of the array
-*/
-char *initMemory(int size)
-{
-	int i;
-	char *array;
-
-	array = malloc((size + 2) * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-
-
-	for (i = 0; i < size + 2; i++)
-	{
-		array[i] = '0';
-	}
-
-	return (array);
+	return (1);
 }
 /**
- * multiply - here where the magic happens
- * this function takes num1 and num2 which are the stirng passed as argv
- * it loop trough every character and multiply it with the other one
- * and push the result of all into the array that i initMemory above
- * @num1: is the first argument
- * @num2: is the second argument
- * @array: is an array of zeros i initalize it above
- * Return: nothing
-*/
-char  *multiply(char *num1, char *num2, char *array)
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
 {
-	int p, i, j, len1 = strlen(num1), len2 = strlen(num2);
+	int i = 0;
 
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			p = (num1[i] - '0') * (num2[j] - '0') + (array[i + j + 1] - '0');
-			array[i + j + 1] = p % 10 + '0';
-			array[i + j] += p / 10;
-		}
+	while (s[i] != '\0')
+	{i++;
 	}
-
-
-	for (i = 0; i < len1 + len2; i++)
-	{
-		if (array[i] != '0')
-			return (&array[i]);
-	}
-	return ("0");
+	return (i);
 }
-
 /**
- * main - this where the program call all the functions
- * @argc: length of arguments passed
- * @argv: an array of arguments passed
- * Return: 0
-*/
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int len1, len2;
-	char *array;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	len1 = strlen(argv[1]) - 1;
-	len2 = strlen(argv[2]) - 1;
-
-	checkNum(argv[1], argv[2]);
-	array = initMemory(len1 + len2);
-	printf("%s\n", multiply(argv[1], argv[2], array));
-	free(array);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
-
 }
 
